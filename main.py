834f6448d20d677b9881c489b9c4ef86315191f5
Mahdi Khashan <mahdikhashan1@gmail.com>
2020-07-10 09:48:48 +0430

    modified:   main.py 	new file:   static/brand/bootstrap-outline.svg 	new file:   static/brand/bootstrap-solid.svg 	new file:   static/css/bootstrap-grid.css 	new file:   static/css/bootstrap-grid.css.map 	new file:   static/css/bootstrap-grid.min.css 	new file:   static/css/bootstrap-grid.min.css.map 	new file:   static/css/bootstrap-reboot.css 	new file:   static/css/bootstrap-reboot.css.map 	new file:   static/css/bootstrap-reboot.min.css 	new file:   static/css/bootstrap-reboot.min.css.map 	new file:   static/css/bootstrap.css 	new file:   static/css/bootstrap.css.map 	new file:   static/css/bootstrap.min.css 	new file:   static/css/bootstrap.min.css.map 	new file:   static/css/signin.css 	new file:   static/js/bootstrap.bundle.js 	new file:   static/js/bootstrap.bundle.js.map 	new file:   static/js/bootstrap.bundle.min.js 	new file:   static/js/bootstrap.bundle.min.js.map 	new file:   static/js/bootstrap.js 	new file:   static/js/bootstrap.js.map 	new file:   static/js/bootstrap.min.js 	new file:   static/js/bootstrap.min.js.map 	modified:   templates/index.html 	new file:   templates/signin.html 	new file:   templates/signup.html

--- main.py
+++ main.py
@@ -6,13 +6,37 @@ from flask import render_template, send_file
 from virgool import Virgool
 from virgoolak import Type1
 
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://e17333e06f6747e9b3d0eb6aed5b6aee@o330581.ingest.sentry.io/5321573",
+    integrations=[FlaskIntegration()]
+)
+
 
 app = Flask(__name__)
+
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
 
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+
+@app.route('/auth/signin')
+def signin():
+    return render_template('signin.html')
+
+
+@app.route('/auth/signup')
+def signup():
+    return render_template('signup.html')
+
 
 @app.route('/api/v1/tag/virgool', methods=['GET'])
 def tag_virgool():
